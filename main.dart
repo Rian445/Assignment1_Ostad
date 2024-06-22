e4ae2e15f595e591e1efeda971e589140b45288f
Rian445 <rianislam445@gmail.com>
2024-06-22 22:21:13 +0600

    Updated

--- main.dart
+++ main.dart
@@ -1,11 +1,8 @@
 class Car {
-
   String brand;
   String model;
   int year;
   double milesDriven;
-
-
   static int numberOfCars = 0;
 
 
@@ -13,17 +10,14 @@ class Car {
     numberOfCars++;
   }
 
-
   void drive(double miles) {
     milesDriven += miles;
   }
-
 
   double getMilesDriven() => milesDriven;
   String getBrand() => brand;
   String getModel() => model;
   int getYear() => year;
-
 
   int getAge() {
     int currentYear = DateTime.now().year;
@@ -32,7 +26,6 @@ class Car {
 }
 
 void main() {
-
   Car car1 = Car('Toyota', 'Corolla', 2015, 50000);
   Car car2 = Car('Honda', 'Civic', 2018, 30000);
   Car car3 = Car('BanglaCar', 'GloryAuto', 2020, 10000);
@@ -46,7 +39,5 @@ void main() {
   print('Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} has driven ${car1.getMilesDriven()} miles and is ${car1.getAge()} years old.');
   print('Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} has driven ${car2.getMilesDriven()} miles and is ${car2.getAge()} years old.');
   print('Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} has driven ${car3.getMilesDriven()} miles and is ${car3.getAge()} years old.');
-
-
   print('Total number of cars created: ${Car.numberOfCars}');
 }
